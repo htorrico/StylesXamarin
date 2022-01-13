353b4bb52a4750d9f5dc06aaa1eaf40b9e271d4f
Hugo Felipe Torrico Marquez <htorrico@tecsup.edu.pe>
2022-01-12 19:49:47 -0500

    Estilos Globales

--- StylesXamarin/StylesXamarin/App.xaml.cs
+++ StylesXamarin/StylesXamarin/App.xaml.cs
@@ -10,7 +10,7 @@ namespace StylesXamarin
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new Page1();
         }
 
         protected override void OnStart()
